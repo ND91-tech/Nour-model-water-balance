e13ebe60b9aa8df1f184f53cad5f663c759f85a8
ND91-tech <dalin@fzp.czu.cz>
2025-08-05 12:53:43 +0200

    Update main.f90

--- main.f90
+++ main.f90
@@ -7,18 +7,18 @@ program nour
 
 
   call init()
-  call compute_all()
+  call compute_rootflux()
+
+  print *, "  cell  hour    ET_flux   Qsurf    Leakage   GWflow    Delta"
+
+  do cell = 1, N_cells
+    do t = 1, n_hours
+
+      print *, cell, t, ET_flux(cell,t), Qsurf_result(cell,t), &
+            L_result(cell,t), Qgw_result(cell,t), deltas(cell,t)
+
+    end do
+  end do
 
 
-         ! Print results
-        print *, "Cell T  ET(mm/day)  Runoff  Leakage  GW  Delta"
-             do cell = 1, N_cells
-                do t = 1, n_days
-                  print *, cell,    t,        ET_flux(cell,t),       Qsurf_result(cell,t), &
-                  L_result(cell,t),      Qgw_result(cell,t),         deltas(cell,t)
-
-                end do
-             end do
-
-             
 end program nour
